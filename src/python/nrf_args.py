ef69257500e8d1af1c8895d0f78bff1ed707261a
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2014-09-14 15:12:59 -0400

    Split nrf argument parsing into a separate file

---
+++ src/python/nrf_args.py
@@ -0,0 +1,39 @@
+from nrf24L01p import NrfPipe
+
+def addNrfArgs(parser):
+  parser.add_argument('-c', '--channel', default=0x04)
+  parser.add_argument('-p0a', '--pipe0address', type=str, default="9a78563412")
+  parser.add_argument('-p0s', '--pipe0size',    type=int, default=4)
+  parser.add_argument('-p1a', '--pipe1address', type=str, default=None)
+  parser.add_argument('-p1s', '--pipe1size',    type=int, default=0)
+  parser.add_argument('-p2a', '--pipe2address', type=str, default=None)
+  parser.add_argument('-p2s', '--pipe2size',    type=int, default=0)
+  parser.add_argument('-p3a', '--pipe3address', type=str, default=None)
+  parser.add_argument('-p3s', '--pipe3size',    type=int, default=0)
+  parser.add_argument('-p4a', '--pipe4address', type=str, default=None)
+  parser.add_argument('-p4s', '--pipe4size',    type=int, default=0)
+  parser.add_argument('-p5a', '--pipe5address', type=str, default=None)
+  parser.add_argument('-p5s', '--pipe5size',    type=int, default=0)
+
+def constructRecAddrPlsize(args):
+  nrfPipes = []
+  for idxPipe in range(6):
+    pipeAddressAttribute =  args.__dict__["pipe%daddress" % (idxPipe)]
+    pipeSizeAttribute = args.__dict__["pipe%dsize" % (idxPipe)]
+    newPipe = None
+    # pipeSizeAttribute: -1 means dunamic, zero means pipe disabled
+    if pipeSizeAttribute != 0:
+      pipeAddress = [int("0x"+pipeAddressAttribute[i:i+2], 0) for i in range(0, len(pipeAddressAttribute), 2)]
+      print "Args: have pipe %d address:" % (idxPipe), pipeAddress
+      print "Args: have pipe %d argument:" % (idxPipe), pipeSizeAttribute
+      payloadSize = None if pipeSizeAttribute == -1 else pipeSizeAttribute
+      print "Args: setting pipe %d payload size to " % (idxPipe), payloadSize
+      newPipe = NrfPipe(address=pipeAddress, payloadSize=payloadSize)
+    else:
+      print "Args: have no pipe %d" % (idxPipe)
+    nrfPipes.append(newPipe)
+
+  print nrfPipes
+  return nrfPipes
+
+
--- src/python/testRead.py
+++ src/python/testRead.py
@@ -4,7 +4,7 @@ import os, sys
 from nrf24L01p import Nrf, Reg, Cmd, NrfPipe
 import select
 import argparse
-
+import nrf_args
 
 
 class Reader:
@@ -24,7 +24,7 @@ class Reader:
     # nrf = Nrf(recAddrPlsize=([0xAE, 0xEF, 0xBE, 0x34, 0x12], 4), channel=channelNum)
     # nrf = Nrf(recAddrPlsize=([0x12, 0x34, 0x56, 0x78, 0x9A], 4))
     # self.nrf = Nrf(recAddrPlsize=([0x9A, 0x78, 0x56, 0x34, 0x12], self.args))
-    self.nrf = Nrf(recAddrPlsize=self.constructRecAddrPlsize(self.args), channel=self.args.channel)
+    self.nrf = Nrf(recAddrPlsize=nrf_args.constructRecAddrPlsize(self.args), channel=self.args.channel)
     # self.nrf = Nrf(recAddrPlsize=[NrfPipe([0x12, 0x34, 0x56, 0x78, 0x9A], 4)], channel=self.args.channel)
     # self.nrf = Nrf(recAddrPlsize=[NrfPipe([0x12, 0x34, 0x56, 0x78, 0x9A], 6)], channel=self.args.channel)
     self.printRegisterMap()
@@ -42,28 +42,6 @@ class Reader:
     while self.read_list:
       if not self.checkProcessInput():
         self.readPrintOutput()
-
-  @staticmethod
-  def constructRecAddrPlsize(args):
-    nrfPipes = []
-    for idxPipe in range(6):
-      pipeAddressAttribute =  args.__dict__["pipe%daddress" % (idxPipe)]
-      pipeSizeAttribute = args.__dict__["pipe%dsize" % (idxPipe)]
-      newPipe = None
-      # pipeSizeAttribute: -1 means dunamic, zero means pipe disabled
-      if pipeSizeAttribute != 0:
-        pipeAddress = [int("0x"+pipeAddressAttribute[i:i+2], 0) for i in range(0, len(pipeAddressAttribute), 2)]
-        print "Args: have pipe %d address:" % (idxPipe), pipeAddress
-        print "Args: have pipe %d argument:" % (idxPipe), pipeSizeAttribute
-        payloadSize = None if pipeSizeAttribute == -1 else pipeSizeAttribute
-        print "Args: setting pipe %d payload size to " % (idxPipe), payloadSize
-        newPipe = NrfPipe(address=pipeAddress, payloadSize=payloadSize)
-      else:
-        print "Args: have no pipe %d" % (idxPipe)
-      nrfPipes.append(newPipe)
-
-    print nrfPipes
-    return nrfPipes
 
   def checkProcessInput(self):
     ready = select.select(self.read_list, [], [], self.readTimeout)[0]
@@ -126,19 +104,7 @@ class Reader:
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-c', '--channel', default=0x04)
-  parser.add_argument('-p0a', '--pipe0address', type=str, default="9a78563412")
-  parser.add_argument('-p0s', '--pipe0size',    type=int, default=4)
-  parser.add_argument('-p1a', '--pipe1address', type=str, default=None)
-  parser.add_argument('-p1s', '--pipe1size',    type=int, default=0)
-  parser.add_argument('-p2a', '--pipe2address', type=str, default=None)
-  parser.add_argument('-p2s', '--pipe2size',    type=int, default=0)
-  parser.add_argument('-p3a', '--pipe3address', type=str, default=None)
-  parser.add_argument('-p3s', '--pipe3size',    type=int, default=0)
-  parser.add_argument('-p4a', '--pipe4address', type=str, default=None)
-  parser.add_argument('-p4s', '--pipe4size',    type=int, default=0)
-  parser.add_argument('-p5a', '--pipe5address', type=str, default=None)
-  parser.add_argument('-p5s', '--pipe5size',    type=int, default=0)
+  nrf_args.addNrfArgs(parser)
   r = Reader(parser.parse_args())
   r.run()
 
